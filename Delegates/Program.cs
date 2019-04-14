6230c60ee4b2fbb00b76854acead1c277d4e7968
Sourav Saha <sourav90.saha@outlook.com>
2019-04-14 13:04:46 +0530

    Add new solution

---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void HelloFunc(string msg);
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            HelloFunc del = new HelloFunc(Hello);
+            del("I am Sourav Saha");
+        }
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Delegates/UseOfDelegate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class UseOfDelegate
+    {
+        static void Main()
+        {
+            List<Employee> lst = new List<Employee>();
+            lst.Add(new Employee() { ID = 1, Name = "Sourav", Sal = 5000, Exp = 5 });
+            lst.Add(new Employee() { ID = 2, Name = "Atanu", Sal = 3000, Exp = 2 });
+            lst.Add(new Employee() { ID = 3, Name = "Debonil", Sal = 4000, Exp = 7 });
+            lst.Add(new Employee() { ID = 4, Name = "Deep", Sal = 6000, Exp = 3 });
+            lst.Add(new Employee() { ID = 4, Name = "Mimo", Sal = 6000, Exp = 3 });
+            //Employee.getPromotion(lst);
+            //Promotion del = new Promotion(Employee.getPromotion);
+            //del(lst);
+            //IsPromotable del = new IsPromotable(condition);
+            //Employee.getPromotion(lst,del);
+            Employee.getPromotion(lst, x => x.Exp >= 5);
+        }
+        //public static bool condition(Employee emp)
+        //{
+        //    if (emp.Exp >= 5)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+    }
+
+    //delegate void Promotion(List<Employee> emplst);
+    delegate bool IsPromotable(Employee emp);
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Sal { get; set; }
+        public int Exp { get; set; }
+
+        public static void getPromotion(List<Employee> emplist, IsPromotable isPromotable)
+        {
+            foreach(Employee emp in emplist)
+            {
+                //if(emp.Exp>=5)
+                if (isPromotable(emp))
+                {
+                    Console.WriteLine(emp.Name + " is Promoted.");
+                }
+            }
+            Console.ReadLine();
+        } 
+    }
+}
